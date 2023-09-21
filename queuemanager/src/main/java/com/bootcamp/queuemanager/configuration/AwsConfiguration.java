c9e3239e37733bd13c9bc0da0e7b45f23e54b162
Jenni-adm <jcarolinne11@gmail.com>
2023-09-20 21:22:02 -0300

    [feat]: Envio de Feedback para as 3 filas

--- queuemanager/src/main/java/com/bootcamp/queuemanager/configuration/AwsConfiguration.java
+++ queuemanager/src/main/java/com/bootcamp/queuemanager/configuration/AwsConfiguration.java
@@ -1,14 +1,13 @@
 package com.bootcamp.queuemanager.configuration;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AwsConfiguration {
@@ -18,13 +17,18 @@ public class AwsConfiguration {
     @Value("${aws.secretKey}")
     private String awsSecretKey;
 
-    @Bean
-    public AmazonSNS amazonSNS() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    @Value("${cloud.aws.region}")
+    private String awsRegion;
 
-        return AmazonSNSClient.builder()
-                .withRegion(Regions.US_EAST_1)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+    @Bean
+    public SnsClient snsClient() {
+
+        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(awsAccessKey, awsSecretKey));
+
+        return SnsClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
--- queuemanager/src/main/java/com/bootcamp/queuemanager/service/FeedbackService.java
+++ queuemanager/src/main/java/com/bootcamp/queuemanager/service/FeedbackService.java
@@ -1,15 +1,10 @@
 package com.bootcamp.queuemanager.service;
 
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.model.PublishResult;
 import com.bootcamp.queuemanager.model.CustomerFeedback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-//import com.amazonaws.services.sns.model.PublishRequest;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
@@ -19,8 +14,6 @@ import software.amazon.awssdk.services.sns.model.PublishResponse;
 public class FeedbackService {
 
     @Autowired
-    private AmazonSNS amazonSNS;
-
     private SnsClient snsClient;
 
     @Value("${aws.sns.sugestoes-arn}")
@@ -32,6 +25,10 @@ public class FeedbackService {
     @Value("${aws.sns.criticas-arn}")
     private String snsTopicCriticas;
 
+    public FeedbackService(SnsClient snsClient) {
+        this.snsClient = snsClient;
+    }
+
     public void enviaFeedback(CustomerFeedback feedback) {
         String snsArn;
         switch (feedback.getType()){
@@ -41,22 +38,13 @@ public class FeedbackService {
             default -> snsArn = "";
         }
 
-        /*PublishRequest publishRequest = new PublishRequest()
-                .withTopicArn(snsArn)
-                .withMessage(feedback.getMessage());*/
-
         PublishRequest publishRequest = PublishRequest.builder()
                 .topicArn(snsArn)
-                .messageGroupId(feedback.getId())
+                .messageGroupId("DEFAULT")
                 .message(feedback.getMessage())
                 .build();
 
-        buildSnsClient();
         PublishResponse response = snsClient.publish(publishRequest);
-
-
-        //PublishResult publishResult = amazonSNS.publish(publishRequest);
-        System.out.println("ID: " + response.messageId() + " | Mensagem: " + response.toString());
     }
 
     private void buildSnsClient() {
